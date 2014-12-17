c842982c74c81a8933496103f1a8d237b341f102
lerniri <webdev@irina-lerner.com>
2014-12-18 01:26:13 +0300

    page menu related code added

--- www/js/main.js
+++ www/js/main.js
@@ -4,9 +4,9 @@ $(document).ready(init);
 
 function init() {
 
-	//Our code goes here
-
-	//Let's draw a path
+	//==================================
+	//			SCROLLING PATH
+	//==================================
 	$.fn.scrollPath("getPath")
 
 		//Home (starting) Page
@@ -63,4 +63,24 @@ function init() {
 			$.fn.scrollPath("scrollTo", target, 1000, "easeInOutSine");
 		});
 	});
+
+
+	$('#cssmenu li.has-sub>a').on('click', function(){
+		$(this).removeAttr('href');
+		var element = $(this).parent('li');
+		if (element.hasClass('open')) {
+			element.removeClass('open');
+			element.find('li').removeClass('open');
+			element.find('ul').slideUp();
+		}
+		else {
+			element.addClass('open');
+			element.children('ul').slideDown();
+			element.siblings('li').children('ul').slideUp();
+			element.siblings('li').removeClass('open');
+			element.siblings('li').find('li').removeClass('open');
+			element.siblings('li').find('ul').slideUp();
+		}
+	});
+
 }
